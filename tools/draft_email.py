02531b612765cde96dddd4746c917aee7a8d6d71
shan501 <shanyuzhang123@gmail.com>
2025-05-02 15:58:01 -0700

    Update drafts agent

--- tools/draft_email.py
+++ tools/draft_email.py
@@ -1,2 +1,37 @@
-def draft_email(args: dict) -> dict:
-    return args
+from arcadepy import Arcade
+
+client = Arcade()
+
+user_id = "aeruku1@gmail.com"
+
+
+auth_response = client.tools.authorize(
+    tool_name="Google.WriteDraftEmail",
+    user_id=user_id,
+)
+
+
+def draft_email(args: dict) -> dict:        
+    client.auth.wait_for_completion(auth_response)
+
+    tool_input = {
+        "recipient": args["recipient_email"],
+        "subject": args["subject"],
+        "body": args["body"],
+    }
+
+    print(tool_input)
+
+    response = client.tools.execute(
+        tool_name="Google.WriteDraftEmail",
+        input=tool_input,
+        user_id=user_id,
+    )
+
+    print(response)
+
+    return {
+        "response": response
+    }
+
+
